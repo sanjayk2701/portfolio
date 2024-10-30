977e0d447d3a35e70e43c45153c7178c2b839b91
sanjayk2701 <sanjaykalidass27@gmail.com>
2024-10-30 12:25:10 +0530

    feat: add timout in db

--- utils/connectMongodb.js
+++ utils/connectMongodb.js
@@ -1,6 +1,17 @@
 import mongoose from "mongoose";
 
-const connectMongodb = async () => mongoose.connect(process.env.MONGODB_URI)
-
+const connectMongodb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 10 seconds
+      socketTimeoutMS: 45000, // 45 seconds
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
 
 export default connectMongodb;
