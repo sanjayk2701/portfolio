a15f1c54e8d007423a5fd279d492c00d2cab17d9
sanjayk2701 <sanjaykalidass27@gmail.com>
2024-10-30 12:37:26 +0530

    fix:db

--- utils/connectMongodb.js
+++ utils/connectMongodb.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const connectMongodb = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // Removed deprecated options
       serverSelectionTimeoutMS: 10000, // 10 seconds
       socketTimeoutMS: 45000, // 45 seconds
     });
