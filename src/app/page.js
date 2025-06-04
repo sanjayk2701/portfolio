85a70da4c28a2a2cd20555f9a69d5ab9900d8150
sanjayk2701 <sanjaykalidass27@gmail.com>
2025-06-05 02:48:52 +0530

    preloader sessionstorage fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Aboutme from "./pages/aboutme/page";
 import Projects from "./pages/projects/page";
@@ -7,38 +7,64 @@ import HeroPage from "./pages/heroPage/page";
 import EducationOverview from "./pages/education/page";
 import Contact from "./pages/contact/page";
 import Footer from "./components/Footer/page";
+import { PropagateLoader } from "react-spinners";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function main() {
+  const [preloading, setPreLoading] = useState(true);
+
+ useEffect(() => {
+  const hasVisited = sessionStorage.getItem("hasVisited");
+  if (hasVisited) {
+    setPreLoading(false); // skip preloader
+  } else {
+    sessionStorage.setItem("hasVisited", "true");
+    const timer = setTimeout(() => {
+      setpreLoading(false);
+    }, 1500); // show preloader on first visit
+
+    return () => clearTimeout(timer);
+  }
+}, []);
+
+  if (preloading) {
+    return (
+      <div className="h-screen w-full bg-black flex flex-col justify-center items-center">
+        <p className="text-2xl text-white mb-4 animate-pulse">
+          Assembling components, please wait...
+        </p>
+        <PropagateLoader color="#ffffff" size={15} />
+      </div>
+    );
+  }
+
   return (
-
     <div>
-
       <section id="hero">
         <HeroPage />
       </section>
-      
+
       <section id="about">
         <Aboutme />
       </section>
-      
+
       <section id="projects">
         <Projects />
       </section>
-      
+
       <section id="skills">
         <Skills />
       </section>
-      
+
       <section id="education">
         <EducationOverview />
       </section>
-      
+
       <section id="contact">
         <Contact />
       </section>
-      
+
       <Footer />
     </div>
   );
