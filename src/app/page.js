6120dba93c539eb5603dd837cc2520edebd7f125
sanjayk2701 <sanjaykalidass27@gmail.com>
2025-06-05 03:35:06 +0530

    undo preloader

--- src/app/page.js
+++ src/app/page.js
@@ -14,30 +14,30 @@ import { useState, useEffect } from "react";
 export default function main() {
   const [preloading, setPreLoading] = useState(true);
 
- useEffect(() => {
-  const hasVisited = sessionStorage.getItem("hasVisited");
-  if (hasVisited) {
-    setPreLoading(false); // skip preloader
-  } else {
-    sessionStorage.setItem("hasVisited", "true");
-    const timer = setTimeout(() => {
-      setpreLoading(false);
-    }, 1500); // show preloader on first visit
+//  useEffect(() => {
+//   const hasVisited = sessionStorage.getItem("hasVisited");
+//   if (hasVisited) {
+//     setPreLoading(false); // skip preloader
+//   } else {
+//     sessionStorage.setItem("hasVisited", "true");
+//     const timer = setTimeout(() => {
+//       setpreLoading(false);
+//     }, 1500); // show preloader on first visit
 
-    return () => clearTimeout(timer);
-  }
-}, []);
+//     return () => clearTimeout(timer);
+//   }
+// }, []);
 
-  if (preloading) {
-    return (
-      <div className="h-screen w-full bg-black flex flex-col justify-center items-center">
-        <p className="text-2xl text-white mb-4 animate-pulse">
-          Assembling components, please wait...
-        </p>
-        <PropagateLoader color="#ffffff" size={15} />
-      </div>
-    );
-  }
+//   if (preloading) {
+//     return (
+//       <div className="h-screen w-full bg-black flex flex-col justify-center items-center">
+//         <p className="text-2xl text-white mb-4 animate-pulse">
+//           Assembling components, please wait...
+//         </p>
+//         <PropagateLoader color="#ffffff" size={15} />
+//       </div>
+//     );
+//   }
 
   return (
     <div>
